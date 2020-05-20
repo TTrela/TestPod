380729f62ee936d5e95f01897919630b68dc9a2c
Tomasz Trela <>
2020-05-20 16:53:10 +0200

    done

---
+++ TestPod/TestPod/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestPod
+//
+//  Created by Tomasz Trela on 20/05/2020.
+//  Copyright © 2020 SLabs. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func sayIt() -> String {
+        return "I did it!"
+    }
+}
---
+++ TestPod/TestPod/TestPod.h
@@ -0,0 +1,19 @@
+//
+//  TestPod.h
+//  TestPod
+//
+//  Created by Tomasz Trela on 20/05/2020.
+//  Copyright © 2020 SLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPod.
+FOUNDATION_EXPORT double TestPodVersionNumber;
+
+//! Project version string for TestPod.
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPod/PublicHeader.h>
+
+
